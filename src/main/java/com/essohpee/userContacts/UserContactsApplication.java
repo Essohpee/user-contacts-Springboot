104bfbc11b373993b59c87b78fb5adff17e99d32
Samuel Overs Pelham <pelham4lib.2018@gmail.com>
2022-02-11 12:14:55 +0800

    Initial commit.

---
+++ src/main/java/com/essohpee/userContacts/UserContactsApplication.java
@@ -0,0 +1,13 @@
+package com.essohpee.userContacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserContactsApplication.class, args);
+	}
+
+}
